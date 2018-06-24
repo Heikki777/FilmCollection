afbc1103dda9756ab80ba16c265815ec2c1ffd8c
Heikki777 <30908923+Heikki777@users.noreply.github.com>
2018-06-25 00:55:15 +0300

    Core Data added

---
+++ FilmCollection/Models/Settings.swift
@@ -0,0 +1,34 @@
+//
+//  Settings.swift
+//  FilmCollection
+//
+//  Created by Heikki Hämälistö on 24/06/2018.
+//  Copyright © 2018 Heikki Hämälistö. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Settings: NSManagedObject{
+    var filmCollectionLayout: FilmCollectionLayoutOption = .posterTitleOverview
+
+    var dictionary: [String: [String]]{
+        return [
+            SectionTitle.FilmCollectionLayout.rawValue: FilmCollectionLayoutOption.all.map{ $0.rawValue }
+        ]
+    }
+    
+    enum SectionTitle: String{
+        case FilmCollectionLayout = "Film collection layout"
+    }
+}
+
+enum FilmCollectionLayoutOption: String{
+    case poster = "Poster"
+    case posterTitleOverview = "Poster, title and overview"
+    case title = "Title"
+
+    static var all: [FilmCollectionLayoutOption]{
+        return [.poster, .posterTitleOverview, .title]
+    }
+}
